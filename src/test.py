942bac05f17660fe4c06b82689443891b38b01ac
IoannisIn <intzes.ioannis@gmail.com>
2023-04-17 23:19:15 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,8 +3,5 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
-async def pwm(dut):
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-    
-    await ClockCycles(dut.clk, 8000)
+async def test_my_design(dut):
+    dut._log.info("start")
