eb76a1a62e16c75fb4d285ca2b14805e594c73af
IoannisIn <intzes.ioannis@gmail.com>
2023-04-17 22:53:05 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -7,8 +7,4 @@ async def pwm(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
     
-    dut.rst.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst.value = 0
-
     await ClockCycles(dut.clk, 8000)
