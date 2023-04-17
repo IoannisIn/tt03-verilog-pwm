1c047bf6d94068bb113cf0ea8a0e45a8234b2792
IoannisIn <intzes.ioannis@gmail.com>
2023-04-17 23:26:18 +0300

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -1,36 +1,24 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
-*/
-
 module tb (
-    // testbench is controlled by test.py
     input clk,
     input btn_incrPWM,
-	input btn_decrPWM,
+    input btn_decrPWM,
     output inled,
-	output deled,
-	output led,
-	output clock_1hz
-   );
+    output deled,
+    output led,
+    output clock_1hz
+    );
 
-    // this part dumps the trace to a vcd file that can be viewed with GTKWave
     initial begin
         $dumpfile ("tb.vcd");
         $dumpvars (0, tb);
         #1;
     end
 
-    // wire up the inputs and outputs
     wire [7:0] inputs = {5'b0, btn_decrPWM, btn_incrPWM, clk};
-    wire [7:0] outputs;
-    assign inled = outputs[0];
-    assign deled = outputs[1];
-    assign led = outputs[2];
-    assign clock_1hz = outputs[3];
+    wire [7:0] outputs = {4'b0, inled, deled, led, clock_1hz};
 
     // instantiate the DUT
     pwm dut(
